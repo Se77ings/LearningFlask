173442fb94b6241ab5c53081fa99cd36e5bfbd27
Se77ings <se77ings@gmail.com>
2023-05-22 13:28:01 -0300

    VERSAO_3

--- app/controllers/default.py
+++ app/controllers/default.py
@@ -1,14 +1,12 @@
 from app import app
+from flask import render_template
 
-@app.route("/index")
-@app.route("/")
-def index():
-    return "Hello World!"
+@app.route("/index/<user>")
+@app.route("/", defaults={"user":None})
+def index(user):
+    return render_template('index.html', user=user)
+#aqui nao precisa passar o diretório porque por padrão ele vai na pasta templates
 
-@app.route("/test")
-@app.route("/test/<name>")
-def teste(name=None):
-    if name:
-        return "Olá, %s!" % name
-    else:
-        return "Olá, Usuário"
+@app.route("/login")
+def login():
+    return render_template('base.html')
